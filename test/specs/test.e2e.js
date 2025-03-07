1a44b1c5fc7e49241f3188af4c347bbb630e02bb
ingaSiu <ingasiud@gmail.com>
2025-03-07 10:58:16 +0200

    skip test

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,15 +1,14 @@
-import { expect } from '@wdio/globals'
-import LoginPage from '../pageobjects/login.page.js'
-import SecurePage from '../pageobjects/secure.page.js'
+import LoginPage from '../pageobjects/login.page.js';
+import SecurePage from '../pageobjects/secure.page.js';
+import { expect } from '@wdio/globals';
 
 describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open()
+  it.skip('should login with valid credentials', async () => {
+    await LoginPage.open();
 
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!')
-        await expect(SecurePage.flashAlert).toBeExisting()
-        await expect(SecurePage.flashAlert).toHaveText(
-            expect.stringContaining('You logged into a secure area!'))
-    })
-})
+    await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+    await expect(SecurePage.flashAlert).toBeExisting();
+    await expect(SecurePage.flashAlert).toHaveText(expect.stringContaining('You logged into a secure area!'));
+  });
+});
 
