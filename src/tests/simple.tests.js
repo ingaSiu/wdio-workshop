d112c59cb674989bd6fe588a9b369d372651aacf
ingaSiu <ingasiud@gmail.com>
2025-03-07 11:58:07 +0200

    create a test for check if modal  opens

--- src/tests/simple.tests.js
+++ src/tests/simple.tests.js
@@ -1,11 +1,24 @@
 describe('Doctors page', () => {
   beforeEach(async () => {
-    await browser.url('https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/doctors');
+    await browser.url('https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/dashboard');
   });
 
   it('Check page title', async () => {
     // const title = await browser.getTitle();
     // console.log(title);
     await expect(browser).toHaveTitle('Appointment Planner - Syncfusion Angular Components Showcase App');
+  });
+
+  it('Open modal for adding a new doctor', async () => {
+    // click on doctors item in the side menu
+    // click on add new doctor button - locate it by a unique identifier first
+    // check that a modal window is displayed
+
+    const myButton = await $('[routerlink="/doctors"]');
+    await myButton.click();
+
+    await $('.specialization-types button.e-control').click();
+
+    await expect($('.new-doctor-dialog .e-dlg-modal')).toBeDisplayed();
   });
 });
