606c0cfd5763145f43e3bc0bb9897fda03e8fba1
ingaSiu <ingasiud@gmail.com>
2025-03-07 13:35:55 +0200

    create a dashboard page object and add an open method to it

---
+++ src/po/pages/dashboard.page.js
@@ -0,0 +1,6 @@
+class DashboardPage {
+  async open() {
+    await browser.url('https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/dashboard');
+  }
+}
+module.exports = DashboardPage;
