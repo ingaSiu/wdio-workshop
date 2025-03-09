dec7cd4a4e75d8876d7c31ecb12ec9e4ac970ae8
ingaSiu <ingasiud@gmail.com>
2025-03-09 09:40:00 +0200

    move sidemenu form dashboard to base  page

--- src/po/pages/base.page.js
+++ src/po/pages/base.page.js
@@ -1,9 +1,10 @@
 const Header = require('../components/common/header.component');
-
+const SideMenuComponent = require('../components/common/sidemenu.component');
 class BasePage {
   constructor(url) {
     this.url = url;
     this.header = new Header();
+    this.sideMenu = new SideMenuComponent();
   }
   open() {
     return browser.url(this.url);
